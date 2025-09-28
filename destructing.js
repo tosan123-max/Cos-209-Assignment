ef40a0b4a1e7448ad72b7f85f0484be550da0747
tosan123-max <amadedont4770@student.baBcock.edu.ng>
2025-09-28 23:55:17 +0100

    Update destructing.js

--- destructing.js
+++ destructing.js
@@ -1,4 +1,4 @@
-const user = { id: 1, username: "Daniel", email: "dachima@gmail.com"};
+const user = { id: 1, username: "Tsolaye", email: "Tsolaye@gmail.com"};
 const{ id, username} = user;
 
 const numbers = [35, 44, 56];
@@ -7,4 +7,5 @@ const [first, second] = numbers;
 console.log("User ID: ", id);
 console.log("Username: ", username);
 console.log("First : ", first, "Second: ", second);
+
 console.log(user)
